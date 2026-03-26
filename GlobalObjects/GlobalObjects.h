b0f699c0e26cfb6bf030b58fd128e0a6bb6a04a1
Adam <gillesad@gmail.com>
2026-03-26 14:04:00 +0000

    Renamed to globalobjects

--- GlobalObjects/GlobalObjects.h
+++ GlobalObjects/GlobalObjects.h
@@ -4,8 +4,13 @@
 #include <Arduino_FreeRTOS.h>
 // - RTOS OBJECTS -
 extern QueueHandle_t commandQueue;
-
+extern SemaphoreHandle_t sensorSemaphore;
 // - STRUCS -
+struct Sensors{
+  uint16_t left = 0;
+  uint16_t right = 0;
+} sensors;
+
 struct SpeedPercentages {
   // Current percentage to feed to driver
   float left = 0;
@@ -19,7 +24,7 @@ struct PIDCoefficients {
   float kd = 0.0;
 } straightening, matching;
 
-struct SpeedArrays{
+struct Arrays{
   // Time and speed arrays
   static constexpr uint8_t ARRAY_SIZE = 5;
   unsigned int targetTimes[ARRAY_SIZE];
@@ -27,7 +32,7 @@ struct SpeedArrays{
 
   unsigned int actualTimes[ARRAY_SIZE];
   unsigned int actualSpeeds[ARRAY_SIZE];
-} speeds;
+} data;
 
 // EEPROM addresses
 constexpr unsigned int LEFT_PERCENTAGE_ADDRESS = 0;
@@ -37,7 +42,5 @@ constexpr unsigned int KI_ADDRESS = KP_ADDRESS + sizeof(float);
 constexpr unsigned int KD_ADDRESS = KI_ADDRESS + sizeof(float);
 
 struct DriverState{
-
   bool stopped = true;
-
 } state;
