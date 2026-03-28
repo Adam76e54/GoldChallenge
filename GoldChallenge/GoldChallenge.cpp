71fef9c7dd5e6b863a18f9106ef59e87477457d5
Adam <gillesad@gmail.com>
2026-03-28 16:22:47 +0000

    Updated to match.ino sketch

--- GoldChallenge/GoldChallenge.cpp
+++ GoldChallenge/GoldChallenge.cpp
@@ -1,14 +1,23 @@
 #include "Buggy.h"
 
-QueueHandle_t commandQueue;
 
-void setup(){
+void setup() {
+  Serial.begin(115200);
+
+  comm::initialiseHandlerTable();
+
   commandQueue = xQueueCreate(10, sizeof(Command));
   configASSERT(commandQueue != nullptr);
 
+  stoppedSemaphore = xSemaphoreCreateBinary();
+  xSemaphoreGive(stoppedSemaphore);
+  
   xTaskCreate(telemetry, "Telemetry", 1024, nullptr, 1, nullptr);
+  xTaskCreate(move, "Move", 1024, nullptr, 2, &moveTaskHandle);
+  xTaskCreate(irSensing, "Sense from IRs", 1024, nullptr, 3, nullptr);
+
+  vTaskStartScheduler();
 }
 
-void loop(){
-  
+void loop() {
 }
