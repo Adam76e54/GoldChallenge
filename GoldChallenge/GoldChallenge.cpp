4a3fc45e9daaf2c41a819049f6b92f0391558114
Adam <gillesad@gmail.com>
2026-04-02 13:10:26 +0100

    Added task handle for telemetry

--- GoldChallenge/GoldChallenge.cpp
+++ GoldChallenge/GoldChallenge.cpp
@@ -5,20 +5,20 @@ ROB12629 leftEncoder(2), rightEncoder(3);
 void setup() {
   Serial.begin(115200);
 
+  leftEncoder.begin(leftISR);
+  rightEncoder.begin(rightISR);
+
   comm::initialiseHandlerTable();
 
-  commandQueue = xQueueCreate(10, sizeof(Command));
-  configASSERT(commandQueue != nullptr);
-
-  stoppedSemaphore = xSemaphoreCreateBinary();
-  xSemaphoreGive(stoppedSemaphore);
+  initialiseSemaphores();
   
-  xTaskCreate(telemetry, "Telemetry", 1024, nullptr, 1, nullptr);
-  xTaskCreate(move, "Move", 1024, nullptr, 2, &moveTaskHandle);
-  xTaskCreate(irSensing, "Sense from IRs", 1024, nullptr, 3, nullptr);
+  xTaskCreate(telemetry, "Telemetry", 512, nullptr, 1, &telemetryTaskHandle);
+  xTaskCreate(decide, "Make decision to move", 256, nullptr, 2, nullptr);
+  xTaskCreate(irSensing, "Sense from IRs", 256, nullptr, 3, nullptr);
 
   vTaskStartScheduler();
 }
 
 void loop() {
+
 }
