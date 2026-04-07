3310439601ab422f55c6b2c6937e37b576817432
Adam <gillesad@gmail.com>
2026-04-07 17:12:41 +0100

    Merge telemetry

--- GoldChallenge/GoldChallenge.cpp
+++ GoldChallenge/GoldChallenge.cpp
@@ -1,19 +1,15 @@
 #include "Buggy.h"
 
-
 void setup() {
   Serial.begin(115200);
-
-  leftEncoder.begin(leftISR);
-  rightEncoder.begin(rightISR);
 
   comm::initialiseHandlerTable();
 
   initialiseSemaphores();
   
   xTaskCreate(telemetry, "Telemetry", 512, nullptr, 1, &telemetryTaskHandle);
-  xTaskCreate(decide, "Make decision to move", 256, nullptr, 2, nullptr);
-  xTaskCreate(irSensing, "Sense from IRs", 256, nullptr, 3, nullptr);
+  xTaskCreate(decide, "Make decision to move", 256, nullptr, 3, nullptr);
+  xTaskCreate(irSensing, "Sense from IRs", 256, nullptr, 2, nullptr);
 
   vTaskStartScheduler();
 }
